a387d2251acc0f737f24fecae36f4d6dfed66384
Edwin-Papasito <nicolas.tarazona.morales@gmail.com>
2021-09-30 15:44:04 -0500

    interfaz

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,21 @@
+#zona de import
+import pygame
 
+
+
+#crear la interfaz grafica
+pygame.init()
+
+VENTA_ANCHO = 600
+VENTANA_LARGO = 600
+VENTANA = pygame.display.set_mode((VENTA_ANCHO,VENTANA_LARGO))
+pygame.display.set_caption("Mesa de Blackjack")
+
+run_game = True
+
+while run_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_game = False #para cerrarlo solo es darle a la X
+
+pygame.quit()
