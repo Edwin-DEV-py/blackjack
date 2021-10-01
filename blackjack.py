9467787511e53482fe39c0d6fe669db9c00df54f
Edwin-Papasito <nicolas.tarazona.morales@gmail.com>
2021-09-30 21:01:20 -0500

    fondo de pantalla

--- blackjack.py
+++ blackjack.py
@@ -1,15 +1,17 @@
 #zona de import
 import pygame
+from pygame import *
 
 
 
 #crear la interfaz grafica
 pygame.init()
 
-VENTA_ANCHO = 600
-VENTANA_LARGO = 600
-VENTANA = pygame.display.set_mode((VENTA_ANCHO,VENTANA_LARGO))
+VENTANA = pygame.display.set_mode([710,440])
+tiempo = pygame.time.Clock()
 pygame.display.set_caption("Mesa de Blackjack")
+
+fondo = pygame.image.load("fondo.png").convert()
 
 run_game = True
 
@@ -17,5 +19,10 @@ while run_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run_game = False #para cerrarlo solo es darle a la X
+    
+    VENTANA.blit(fondo,[0,0])
+
+    pygame.display.flip()
+    tiempo.tick(60)
 
 pygame.quit()
