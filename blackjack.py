147a34ff80cc71fba2137158efd190fb21c4e471
Acux-dev <juansup123@gmail.com>
2021-10-03 13:28:23 -0500

    texto

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,10 @@
 #zona de import
-from _typeshed import Self
+from typing import Sequence
 import pygame
 from pygame import *
 from enum import Enum,IntEnum
 import random
+
 
 
 
@@ -12,70 +13,53 @@ pygame.init()
 
 VENTANA = pygame.display.set_mode([710,440])
 tiempo = pygame.time.Clock()
+ganar_WIDTH = 710
+ganar_HEIGHT = 440
+ganar = pygame.display.set_mode((ganar_WIDTH, ganar_HEIGHT))
 pygame.display.set_caption("Mesa de Blackjack")
 
 fondo = pygame.image.load("fondo.png").convert()
 
-run_game = True
-
-while run_game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run_game = False #para cerrarlo solo es darle a la X
-    
-    VENTANA.blit(fondo,[0,0])
-
-    pygame.display.flip()
-    tiempo.tick(60)
-
-pygame.quit()
-
 ####################Boton#######################
+imagen_mano = pygame.image.load("mano.png").convert_alpha()
+imagen_x = pygame.image.load("pasar.png").convert_alpha()
+#class boton
+class Boton():
+    def __init__(self,x,y,image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
 
-# codigo para implementar boton de inicio 
-# self.play_boton = boton(self, "Play")
+    def dibujar(self):
+        VENTANA.blit(self.image,(self.rect.x, self.rect.y)) #dibujar el boton en la panytalla
+#crear la instancia del boton
+boton_mano = Boton(622,269, imagen_mano)
+boton_x = Boton(622,337, imagen_x)
+##click##################
 
-# codigo para poner la pantalla de jugar en el caso que no se encuentre en partida
-# if not self.juego_activado:
-#     self.play_boton.dibujaboton()
+# Initialize Font and Score Texts
+GUI_font = pygame.font.SysFont(None, 32)
+ganar_font = pygame.font.SysFont(None, 42)
+INST_font = pygame.font.SysFont(None, 16)
+TITLE_font = pygame.font.SysFont(None, 24)
 
-# codigo para evento en el que al precionar play lo envia al juego
-# elif event.type == pygame.MOUSEBUTTONDOWN:
-#     mousepos = pygame.mouse.get_pos()
-#     self.checaboton(mousepos)
+ganar_int = 0
+ganar_str = ['', 'PLAYER ganarS', 'AI ganarS', 'PLAYER BUST — AI ganarS', 'PLAYER ganarS — AI BUST', 'TIED', 'NO ganarNERS']
+ganar_x = [0, 100, 65, 180, 180, 40, 100]
+ganar_y = [0, 30, 30, 30, 30, 30, 30]
 
-class Botton:
-    def __init__(self, a_game, texto):
-        self.screen = a_game.screen
-        self.screen_rect = self.screen.get_rect()
-        self.width, self.height = 200, 50
-        self.color = (255,0,0)
-        self.textcolor = (255, 255, 255)
+# Display texts on screen / GUI
+def draw_texts():
+    # Display GUI texts
+    ai_hand_text = GUI_font.render("AI HAND:",True,white)
+    player_hand_text = GUI_font.render("PLAYER HAND:",True,white)
+    hand_value_text = GUI_font.render('HAND VALUE: '+ str(obtener_valor_carta(player_hand)),True,white)
+    ganarner_text = ganar_font.render(ganar_str[ganar_int],True,white)
 
-        self.font = pygame.font.sysfont(None, 48)
-
-        self.rect = pygame.Rect(0,0, self.width, self.height)
-        self.rect.center = self.screen_rect.center
-
-        self.prepara_texto(texto)
-
-#boton inicio
-    def prepara_texto(self, texto):
-        self.texto_image = self.font.render(texto, True, self.textcolor, self.color)
-        self.texto_image_rect = self.texto_image.get_rect()
-        self.texto_image_rect.center = self.rect.center
-
-    def dibujaboton(self):
-        self.screen.fill(self.color, self.rect)
-        self.screen.blit(self.texto_image, self.texto_image_rect)
-    
-#boton de reiniciar cuando el juego no se este ejecutando
-
-    def checaboton(self, mousepos):
-        self.botonp = self.play_boton.rect.colllidepoint(mousepos)
-        if self.botonp and not self.run_game:
-            self.estadistica.reinicia()
-            self.run_game = True
+    ganar.blit(ganarner_text, (ganar_WIDTH//2-ganar_x[ganar_int], ganar_HEIGHT//2-ganar_y[ganar_int]))
+    ganar.blit(hand_value_text, (15,ganar_HEIGHT-CARD_HEIGHT-85))
+    ganar.blit(ai_hand_text, (15,15))
+    ganar.blit(player_hand_text, (15,ganar_HEIGHT-CARD_HEIGHT-60))
 
 
 #################clase carta#######################
@@ -102,8 +86,117 @@ class Dealer():
 
         return deck
 
+
 #########################Clase player#########################
 
 
 
 ########################Repartir cartas######################
+#valores iniciales en las manos del jugador y del IA
+player_hand = []
+IA_hand = []
+##Buscar las cartas dentro del array
+def buscar_carta(valor,suit):
+    if suit == 'pica':
+        T = 1
+    elif suit == 'trebol':
+        T = 2
+    elif suit == 'diamante':
+        T = 3
+    elif suit == 'corazon':
+        T = 4
+    else:
+        print("error")
+    
+    return (valor-1)*4 + (T-1)
+
+ #seleccion de carta al azar
+def obtener_carta_random():
+    global full_deck
+    Random = random.randint(0,len(full_deck)-1)
+
+    return full_deck.pop(Random)
+
+#asiganar una carta en la mano del jugador
+def jugador_elije_carta():
+    player_hand.append(obtener_carta_random())
+
+#asignar carta a la mano del IA
+def IA_elije_carta():
+    if obtener_valor_carta(IA_hand) < 17:
+        IA_hand.append(obtener_carta_random())
+
+        return True
+
+    else:
+        return False
+
+#obtener el valor de las cartas del jugador / IA
+def obtener_valor_carta(mano):
+    if len(mano) == 0: #si la mano esta vacia obtener 8
+        return 0
+    else:
+        Aces = []
+        suma = 0
+
+        for i in mano:
+            if i.simbolo == 1:
+                Aces.append(i)
+            
+            suma += i.valor
+        
+        if suma > 21 and (len(Aces) != 0): #cuando la suma es mas de 21 y hay un As
+            suma -= 10
+        return suma
+##########delaer expand
+original_deck = Dealer()
+
+###########el programa lee las teclas#########
+keys = pygame.key.get_pressed()
+
+#loop#########################################3
+run_game = True
+
+while run_game:
+#evento del mouse
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_game = False #para cerrarlo solo es darle a la X
+        #reiniciar juego
+    if keys[pygame.K_ESCAPE]:
+        full_deck = list(original_deck)
+        run_game = True
+        session = True
+        main_loop = 0
+        ganar_int = 0
+        reveal = False
+        session = True
+        player_hand = []
+        AI_hand = []
+        hidden_hand = []
+    mouse_pos = pygame.mouse.get_pos()
+
+
+        #el jugador decide coger una carta
+    if event.type == pygame.MOUSEBUTTONDOWN:
+         if boton_mano.rect.collidepoint(mouse_pos) and main_loop == 0 and session:
+            jugador_elije_carta()
+            main_loop = 1
+
+            AI_hit = IA_elije_carta()
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if boton_x.rect.collidepoint(mouse_pos):
+            run_game = False
+
+    VENTANA.blit(imagen_mano,[500,200])
+    VENTANA.blit(imagen_x,[622,280])
+    VENTANA.blit(fondo,[0,0])
+    boton_mano.dibujar()
+    boton_x.dibujar()
+    pygame.display.flip()
+    tiempo.tick(60)
+
+############delaer expand#######
+full_deck = list(original_deck)
+
+pygame.quit()
--- pruebas.py
+++ pruebas.py
@@ -1,1 +1,206 @@
-print(list(range(1,14)))
+# Program by Myron Ho
+# ME 4990
+# Final Project
+# Black Jack
+
+# Card Image Source: http://acbl.mybigcommerce.com/52-playing-cards/
+
+import pygame
+from enum import Enum, IntEnum
+import random
+
+
+
+## CUSTOM FUNCTIONS
+
+# Create Card Object
+class Card():
+    def __init__(self,V,N,S,P):
+        self.value = V      # Card Value
+        self.name = N       # Card Name
+        self.suit = S       # Card Suit
+        self.image = P      # Card Picture
+
+
+
+
+
+# Display texts on screen / GUI
+def draw_texts():
+    # Display GUI texts
+    ai_hand_text = GUI_font.render("AI HAND:",True,white)
+    player_hand_text = GUI_font.render("PLAYER HAND:",True,white)
+    hand_value_text = GUI_font.render('HAND VALUE: '+ str(get_card_value(player_hand)),True,white)
+    winner_text = WIN_font.render(win_str[win_int],True,white)
+
+    win.blit(winner_text, (WIN_WIDTH//2-win_x[win_int], WIN_HEIGHT//2-win_y[win_int]))
+    win.blit(hand_value_text, (15,WIN_HEIGHT-CARD_HEIGHT-85))
+    win.blit(ai_hand_text, (15,15))
+    win.blit(player_hand_text, (15,WIN_HEIGHT-CARD_HEIGHT-60))
+
+
+
+
+
+# Initialize Font and Score Texts
+GUI_font = pygame.font.SysFont(None, 32)
+WIN_font = pygame.font.SysFont(None, 42)
+INST_font = pygame.font.SysFont(None, 16)
+TITLE_font = pygame.font.SysFont(None, 24)
+
+win_int = 0
+win_str = ['', 'PLAYER WINS', 'AI WINS', 'PLAYER BUST — AI WINS', 'PLAYER WINS — AI BUST', 'TIED', 'NO WINNERS']
+win_x = [0, 100, 65, 180, 180, 40, 100]
+win_y = [0, 30, 30, 30, 30, 30, 30]
+
+
+# Initialize booleans for GUI mantainence
+main_loop = 0
+run_game = True
+reveal = False
+session = True
+spectate = False
+
+
+
+# Main Game Loop
+while run_game:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # GUI window will remain open until [X] button is clicked or GUI is closed.
+            run_game = False
+
+    # Create background
+    win.fill(black)
+    draw_texts()
+
+    # Used to prevent button spam
+    if main_loop > 0:
+        main_loop += 1
+    if main_loop > 5:
+        main_loop = 0
+
+
+    # Obtain all keys pressed
+    keys = pygame.key.get_pressed()
+
+    # Restart Game
+    if keys[pygame.K_ESCAPE]:
+        # Reinitialize everything
+        full_deck = list(original_deck)
+        run_game = True
+        session = True
+        main_loop = 0
+        win_int = 0
+        reveal = False
+        session = True
+        player_hand = []
+        AI_hand = []
+        hidden_hand = []
+
+    # Player hit / obtain another card
+    if keys[pygame.K_SPACE] and main_loop == 0 and session:
+        player_draw_cards()
+        main_loop = 1
+        
+        AI_hit = AI_draw_card()
+
+        print("AI: ", end='')
+        print(get_card_value(AI_hand))
+
+        # Test all possible outcomes
+        if get_card_value(AI_hand) > 21 and get_card_value(player_hand) > 21:
+            session = False
+            print("NO WINNERS")
+            win_int = 6
+            reveal = True
+        elif get_card_value(AI_hand) > 21:
+            session = False
+            print("AI BUST, PLAYER WINS")
+            win_int = 4
+            reveal = True
+        elif get_card_value(player_hand) > 21:
+            # PLAYER BUST
+            # AI WINS     
+            print('PLAYER BUST, AI WINS')
+            win_int = 3
+            session = False
+            reveal = True
+        elif get_card_value(AI_hand) == 21 and get_card_value(player_hand) == 21:
+            # TIED
+            print('TIED')
+            win_int = 5
+            session = False
+            reveal = True
+        elif get_card_value(AI_hand) == 21 and get_card_value(player_hand) != 21:
+            # AI WINS
+            print('AI WINS')
+            win_int = 2
+            session = False
+            reveal = True
+        elif get_card_value(AI_hand) != 21 and get_card_value(player_hand) == 21:
+            # PLAYER WINS
+            print('PLAYER WINS')
+            win_int = 1
+            session = False
+            reveal = True
+    
+    # Player passes
+    if (keys[pygame.K_KP_ENTER] or keys[pygame.K_RETURN]) and main_loop == 0 and session:
+        main_loop = 1
+
+        AI_hit = AI_draw_card()
+
+        print("AI: ", end='')
+        print(get_card_value(AI_hand))
+
+        # Test all possible outcomes
+        if (AI_hit == False):
+            if get_card_value(AI_hand) > get_card_value(player_hand):
+                session = False
+                print("AI WINS")
+                win_int = 2
+                reveal = True
+            elif get_card_value(AI_hand) < get_card_value(player_hand):
+                session = False
+                print("PLAYER WINS")
+                win_int = 1
+                reveal = True
+            else:
+                session = False
+                print("TIED")
+                win_int = 5
+                reveal = True
+        else:
+            if get_card_value(AI_hand) > 21 and get_card_value(player_hand) > 21:
+                session = False
+                print("NO WINNERS")
+                win_int = 6
+                reveal = True
+            elif get_card_value(AI_hand) > 21:
+                session = False
+                print("AI BUST, PLAYER WINS")
+                win_int = 4
+                reveal = True
+            elif get_card_value(AI_hand) == 21 and get_card_value(player_hand) == 21:
+                # TIED
+                print('TIED')
+                win_int = 5
+                session = False
+                reveal = True
+            elif get_card_value(AI_hand) == 21 and get_card_value(player_hand) != 21:
+                # AI WINS
+                print('AI WINS')
+                win_int = 2
+                session = False
+                reveal = True
+            elif get_card_value(AI_hand) != 21 and get_card_value(player_hand) == 21:
+                # PLAYER WINS
+                print('PLAYER WINS')
+                win_int = 1
+                session = False
+                reveal = True
+
+
+pygame.quit()
